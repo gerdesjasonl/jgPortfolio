6c97d3a6bfaf270850b359ad3590bfa961c32e8f
gerdesjasonl <gerdesjasonl@gmail.com>
2025-02-05 13:06:24 -0500

    Beginning of building pages and linking Nav.

---
+++ jgPortfolio/src/components/NavTabs.jsx
@@ -0,0 +1,11 @@
+function NavTabs({ currentPage, handlePageChange }) {
+    return (<nav id="homeNav">
+            <li href="">ABOUT ME</li>
+            <li href="">PORTFOLIO</li>
+            <li href="">CONTACT</li>
+            <li href="">Resume</li>
+          </nav>
+    )
+}
+
+export default NavTabs;
---
+++ jgPortfolio/src/components/pages/About.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+          <h1 id="homeH1">J.L.GERDES</h1> 
+        </div>
+    );
+}
